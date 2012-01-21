8010639d4f6e0ffa325ed4bb66eea5f10cb13fa0
Marlo Longley <marlo.e.longley@gmail.com>
2012-01-20 17:01:08 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Cinelist::Application.config.secret_token = 'a3dd1b8947b5423e774b04ffaa6dd7218230e10d4fc23434bed156b07f6ad6a943faee9de28c4746156199550c65222bb7676b91d2dfcba54320d82780165e96'
